9fa41a234b24b2932b7e092cc6b979b63a336b38
Laurent Ferreira <laurentalp@example.com>
2026-05-24 10:22:36 -0300

    fix: remove redundant valid_weight_kg.sql duplicate of positive_value test in schema.yml

--- logistica_dbt/tests/valid_weight_kg.sql
+++
@@ -1,7 +0,0 @@
-{{ config(tags=['data_quality']) }}
-
--- Validates that weight_kg is a positive number (greater than zero)
-SELECT *
-FROM {{ ref('stg_shipments') }}
-WHERE weight_kg IS NOT NULL 
-  AND weight_kg <= 0
